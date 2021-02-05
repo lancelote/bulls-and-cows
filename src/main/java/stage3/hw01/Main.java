aaadc71b0ea49b9ff1771acd113f91187761284a
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-02-05 11:31:13 +0300

    Stage 3 homework 1

---
+++ src/main/java/stage3/hw01/Main.java
@@ -0,0 +1,16 @@
+package stage3.hw01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sign(int number) {
+        return Integer.compare(number, 0);
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        System.out.println(sign(n));
+    }
+}

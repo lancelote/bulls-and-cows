875b4972b10cfc5e6db1a97ec21cc570a4b0a92b
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-09-04 11:50:47 +0300

    Homework 9

---
+++ src/main/java/stage5/hw9/Person.java
@@ -0,0 +1,37 @@
+package stage5.hw9;
+
+class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Person(String name, int age, int height, int weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public int compareTo(Person otherPerson) {
+        int res = getName().compareTo(otherPerson.getName());
+        return res != 0 ? res : Integer.compare(getAge(), otherPerson.getAge());
+    }
+}
